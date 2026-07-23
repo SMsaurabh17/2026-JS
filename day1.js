ee34a6211eba81f94e10e9a800e7c3888e5afe82
Saurabh Mahajan <mahajansaurabh55@gmail.com>
2026-07-24 00:11:17 +0530

    changes

--- day1.js
+++ day1.js
@@ -1,1 +1,31 @@
+// variable and type of it 
+
 console.log("hello")
+
+let a = 10 
+console.log(typeof(a))
+
+let name = "saurabh"
+console.log(typeof(name))
+
+let b = true
+console.log(typeof(b))
+
+let c = false
+console.log(typeof(c))
+
+let True = 200
+d = True
+console.log(d)
+console.log(typeof(d))
+
+
+special = "@#$%^&"
+console.log(typeof(special))
+
+const n = 100
+console.log(n)
+
+// TypeError: Assignment to constant variable.
+n = 55
+console.log(n)
