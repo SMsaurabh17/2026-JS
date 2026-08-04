4af38ae9915fe510e88808d4dae13c25a1f456c0
Saurabh Mahajan <mahajansaurabh55@gmail.com>
2026-08-04 17:32:38 +0530

    Added

---
+++ day7.js
@@ -0,0 +1,18 @@
+// Array datatype
+// array can store multiple types of datatypes
+// array is a object that has property and methods
+
+// property
+arr = ["pune","mumbai","banglore","jaipur","delhi"]
+console.log(arr.length)
+console.log(typeof(arr))
+
+// loop on an array
+for(i = 0; i <= arr.length - 1; i++){
+    console.log(arr[i])
+}
+
+// reverse
+for(j = arr.length - 1; j >= 0; j--){
+    console.log(arr[j])
+}
