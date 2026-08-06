6769c576df2b08dc3c43af51f9fcdebba00690d4
Saurabh Mahajan <mahajansaurabh55@gmail.com>
2026-08-06 20:39:12 +0530

    Updated

--- day7.js
+++ day7.js
@@ -16,3 +16,60 @@ for(i = 0; i <= arr.length - 1; i++){
 for(j = arr.length - 1; j >= 0; j--){
     console.log(arr[j])
 }
+
+console.log("--------------------------------------")
+
+// methods on an array
+// push
+let city = ["Jaipur","Pune","Banglore"]
+console.log(city.push("Chennai"))
+console.log(city)
+
+// unshift
+console.log(city.unshift("Kerala"))
+console.log(city)
+
+// pop
+console.log(city.pop())
+console.log(city)
+
+// shift
+console.log(city.shift())
+console.log(city)
+
+// includes
+console.log(city.includes("Pune"))
+console.log(city.includes("Kerala"))
+console.log(city)
+
+// indexof
+console.log(city.indexOf("Banglore"))
+
+// at
+console.log(city.at(1))
+
+// sort
+numb = [89,52,11,33,10,21,75,96]
+console.log(numb.sort())
+
+// reverse
+console.log(numb.reverse())
+
+// concat
+let a = [1,2,3]
+let b = [4,5,6]
+console.log(a.concat(b))
+
+// slice
+let state = ["Andra Pradesh","Madhya Pradesh","Maharashtra","Karnataka","Tamil Nadu","Goa"]
+// start-Index,End-Index where end index is not does not count
+console.log(state.slice(0,2))
+console.log(state.slice(1,3))
+console.log(state.slice(1,5))
+console.log(state.slice(1,-2))
+console.log(state.slice(0,-4))
+console.log(state.slice(-6,-3))
+
+// splices
+// startIndex,numberOfElementToBeDeleted
+// console.log(state.splice(1,3))
